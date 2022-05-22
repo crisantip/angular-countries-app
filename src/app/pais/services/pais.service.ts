3384c74db3c5263684f0acea58e472cd510d054a
Christian Inapanta <cinapanta@cobiscorp.com>
2022-05-22 05:50:03 -0500

    107.2 Implementación alternativa para manejo de errores con operadores de rxjs

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,9 @@ export class PaisService {
   
   buscarPais(termino: string): Observable<any> {
     const url = `${this.baseUrl}/name/${termino}`;
-    return this.http.get(url);
+    return this.http.get(url)
+      .pipe(
+        catchError(err => of(['Hola mundo']))
+      );
    }
 }
